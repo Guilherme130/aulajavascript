85375d59026747d6b8cedb268ea15b973baf6f36
Guilherme <guilherme130447@gmail.com>
2020-05-05 18:27:13 -0300

    curso de introducao ao javascript

---
+++ js/main.js
@@ -0,0 +1,62 @@
+function clique() {
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+    //alert("Você clicou no botão")
+}
+
+function trocar(elemento) {
+    elemento.innerHTML = "Obrigado por passar o mouse"
+}
+
+function voltar(elemento) {
+    elemento.innerHTML = "Passe o mouse aqui"
+}
+
+function funcaochange(elemento) {
+    console.log(elemento.value)
+}
+
+/*function load() {
+    alert("Pagina carregada")
+}
+
+
+/*function soma(n1, n2) {
+    return n1 + n2;
+}
+s = soma(2, 3)
+console.log(s)
+
+/*
+var d = new Date();
+alert(d.getHours());
+
+
+
+/*var idade = prompt("Qual é a sua idade?")
+if (idade >= 18) {
+    alert("Você já é maior de idade")
+} else {
+    alert("Você é menor de idade")
+}
+
+
+
+/*var fruta = [{ nome: "uva", cor: "roxo" }, { nome: "maça", cor: "vermelha" }, { nome: "pera", cor: "verde" }]
+console.log(fruta[1].nome)
+
+//alert("Esta página contém vírus")
+//var nome = "Guilherme José"
+//var idade = 17
+//alert(nome + " tem " + idade + " anos")
+//var idade2 = 10
+/*alert(idade + idade2)
+var frase = "Japão é o melhor time do mundo"
+console.log(frase)
+console.log(frase.replace("Japão", "Brasil"))
+console.log(frase.toUpperCase())
+alert(frase.replace("Japão", "BRA"))
+var lista = ["Maça", "Pêra", "laranja"]
+lista.reverse()
+console.log(lista.length)
+console.log(lista.toString)
+console.log(lista.join(" - "))*/
